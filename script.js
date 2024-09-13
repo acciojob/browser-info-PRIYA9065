b694f7ac3c0f2bc876444ffb552311b186342494
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-09-14 02:09:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+const browser = document.getElementById("browser-info");
+
+browser.innerText = "You are using " + navigator.appName + " version " + navigator.appVersion;
